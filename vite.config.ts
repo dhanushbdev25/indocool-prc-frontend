03ba0ab2a92ead7bc5e47e158f07c809d97b14d3
dhanushbdev25 <dhanushbdev@gmail.com>
2025-12-29 15:25:55 +0530

    fix: replaced https to http in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,9 @@ export default defineConfig({
 		//server environment
 		EnvironmentPlugin({
 			VITE_APP_NAME: 'PRC Management Portal',
-			API_BASE_URL: 'https://43.205.188.89:8080/web/',
-			API_BASE_URL_PRE_AUTH: 'https://43.205.188.89:8080/',
-			REDIRECT_URI: 'https://43.205.188.89:5173/auth/login',
+			API_BASE_URL: 'http://43.205.188.89:8080/web/',
+			API_BASE_URL_PRE_AUTH: 'http://43.205.188.89:8080/',
+			REDIRECT_URI: 'http://43.205.188.89:5173/auth/login',
 			AUTH_MODE: 'localStorage'
 		})
 	]
